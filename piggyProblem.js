46d20020557f24189ee48e7b3fd5c1c22dd36cf8
Eric <eso001@ucr.edu>
2017-01-23 22:00:47 -0800

    Piggy Problem Added

---
+++ piggyProblem.js
@@ -0,0 +1,6 @@
+// one of the buckets has poison. it takes a variable amount of minute for a pig to die and 
+//you get an amount of time to test if they will die. Return how many picks you need minimally
+//to check for the poison bucket
+var poorPigs = function(buckets, minutesToDie, minutesToTest) {
+        
+};
